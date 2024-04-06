b08da71e588de181fc41d1c2ccc7b54d98c90f58
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2024-04-07 05:04:51 +0530

    feat:footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Navbar } from './components/Navbar';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 <Navbar/>
 <Home/>
 <About/>
+<Footer/>
   </div>
   );
 }
